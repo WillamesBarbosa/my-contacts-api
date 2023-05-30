9b197e91411ac84462fc31df4849d0d6a006239d
Willames <willamessilva1999@gmail.com>
2023-05-30 17:09:35 -0300

    refactor: all finds from ctactRepsitory refactored

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -17,22 +17,22 @@ let contacts = [{
 }];
 
 class ContactsRepository {
-  findAll() {
-    return new Promise((resolve) => {
-      resolve(contacts);
-    });
+  async findAll() {
+    const rows = await database.query('SELECT * FROM contacts');
+
+    return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) => {
-      resolve(contacts.find((contact) => contact.id === Number(id)));
-    });
+  async findById(id) {
+    const [row] = await database.query('SELECT * FROM contacts WHERE id = $1', [id]);
+
+    return row;
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) => {
-      resolve(contacts.find((contact) => contact.email === email));
-    });
+  async findByEmail(email) {
+    const [row] = await database.query('SELECT * FROM contacts WHERE email = $1', [email]);
+
+    return row;
   }
 
   async create({
