dd5357b07c4205846d8fe91a75d9538d295f689d
Willames <willamessilva1999@gmail.com>
2023-05-30 15:42:46 -0300

    feat: database created

---
+++ src/database/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE mycontacts;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS categories (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL
+);
