9a2a7203ff71069cac890eb8183c375d3d08ed09
Willames <willamessilva1999@gmail.com>
2023-06-01 16:52:45 -0300

    feat: error handler created

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     'no-console': 'off',
     'class-methods-use-this': 'off',
     camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
 };
--- src/index.js
+++ src/index.js
@@ -15,6 +15,11 @@ const initialTextWhenConnect = `
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log('## ERROR HANDLER ##');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => {
   console.log(initialTextWhenConnect);
